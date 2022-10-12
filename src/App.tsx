2d55b53a18de089192d91d7b6a909f4d5749bc7b
Leonardo Ramos de Sousa <leoramosdsousa@gmail.com>
2022-10-12 11:12:38 -0300

    base structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
-import './App.css'
+import styles from './App.module.css';
+import poweredImage from './assets/powered.png';
 
 export function App() {
   return (
-    <h1>IMC Calculator</h1>
+    <div className={styles.main}>
+      <header>
+        <div className={styles.headerContainer}>
+          <img src={poweredImage} alt="powered logo" width={150} />
+        </div>
+      </header>
+      <div className={styles.container}>
+        <div className={styles.leftSide}>
+          Lado Esquerdo
+        </div>
+        <div className={styles.rightSide}>
+          Lado Direito
+        </div>
+      </div>
+    </div>
   )
 }
 
